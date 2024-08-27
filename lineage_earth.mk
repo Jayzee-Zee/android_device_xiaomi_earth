521eb7341c9555bd4d0b2d453e7a635ae5b66c51
Jayzee-Zee <i.am.hipop@gmail.com>
2024-08-27 21:05:24 +0800

    Update lineage_earth.mk

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -14,6 +14,17 @@ $(call inherit-product, device/xiaomi/earth/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#ngising OS
+RISING_PACKAGE_TYPE :=VANILLA_AOSP
+TARGET_ENABLE_BLUR :=false
+PRODUCT_NO_CAMERA := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+WITH_GMS := false
+TARGET_CORE_GMS := false
+TARGET_CORE_GMS_EXTRAS := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+RISING_MAINTAINER=Jayzee-Zee
+
 PRODUCT_NAME := lineage_earth
 PRODUCT_DEVICE := earth
 PRODUCT_MANUFACTURER := Xiaomi
@@ -24,16 +35,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="earth-user 14 UP1A.230105.007 V816.0.2.0.UCVMIXM release-keys"
-    RISING_CHIPSET :=MT6768
-    RISING_MAINTAINER :=Jayzee-Zee
-    RISING_PACKAGE_TYPE :=VANILLA_AOSP
-    TARGET_ENABLE_BLUR :=false
-    PRODUCT_NO_CAMERA := false
-    TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-    WITH_GMS := false
-    TARGET_CORE_GMS := false
-    TARGET_CORE_GMS_EXTRAS := false
-    TARGET_DEFAULT_PIXEL_LAUNCHER := false
+    RISING_CHIPSET=MT6768
+    RISING_MAINTAINER=Jayzee-Zee
 
 BUILD_FINGERPRINT := Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys
 

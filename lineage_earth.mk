c0d78fe693c10dd0b60b97fe0ffa12b412315cab
Jayzee-Zee <i.am.hipop@gmail.com>
2025-05-29 11:22:51 +0800

    Update lineage_earth.mk

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -26,17 +26,25 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="earth-user 14 UP1A.230105.007 V816.0.2.0.UCVMIXM release-keys"
-    RISING_CHIPSET="Helio G85" \
-    RISING_MAINTAINER="Jayzee"
-    RISING_PACKAGE_TYPE :=VANILLA_AOSP
     TARGET_ENABLE_BLUR :=false
     PRODUCT_NO_CAMERA := false
-    TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-    WITH_GMS := false
-    TARGET_CORE_GMS := false
-    TARGET_CORE_GMS_EXTRAS := false
-    TARGET_DEFAULT_PIXEL_LAUNCHER := false
-    TARGET_BOOT_ANIMATION_RES := 1080
+    TARGET_BOOT_ANIMATION_RES := 720
+    # To include Gapps 
+    WITH_GMS := true
+
+    # To Build Google(Dailer, Message, Phone) and BCR
+    WITH_GMS_COMMS_SUITE := true
+
+    # To Add cinematic wallpaer support (only supported in gapps build not in vanilla remove this flag for vanilla builds)
+    TARGET_SUPPORTS_WALLEFFECT := false
+
+    #Some more GMS Flag
+    TARGET_SUPPORTS_GOOGLE_RECORDER := true
+    TARGET_INCLUDE_STOCK_ARCORE := false
+    TARGET_INCLUDE_STOCK_AICORE := false
+
+    # To Add Bypass Charging Support (need to be adapted in DT and KT as well)
+    BYPASS_CHARGE_SUPPORTED  := false
 
 BUILD_FINGERPRINT := Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys
 
